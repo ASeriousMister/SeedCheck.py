698929266e543b99ad1ac508664020d720ce6463
MisterSerious <43838750+ASeriousMister@users.noreply.github.com>
2022-06-03 16:27:28 +0200

    Add files via upload

---
+++ electrum/gui/qt/bip39_recovery_dialog.py
@@ -0,0 +1,89 @@
+# Copyright (C) 2020 The Electrum developers
+# Distributed under the MIT software license, see the accompanying
+# file LICENCE or http://www.opensource.org/licenses/mit-license.php
+
+import asyncio
+import concurrent.futures
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QLabel, QListWidget, QListWidgetItem
+
+from electrum.i18n import _
+from electrum.network import Network
+from electrum.bip39_recovery import account_discovery
+from electrum.logging import get_logger
+
+from .util import WindowModalDialog, MessageBoxMixin, TaskThread, Buttons, CancelButton, OkButton
+
+
+_logger = get_logger(__name__)
+
+
+class Bip39RecoveryDialog(WindowModalDialog):
+
+    ROLE_ACCOUNT = Qt.UserRole
+
+    def __init__(self, parent: QWidget, get_account_xpub, on_account_select):
+        self.get_account_xpub = get_account_xpub
+        self.on_account_select = on_account_select
+        WindowModalDialog.__init__(self, parent, _('BIP39 Recovery'))
+        self.setMinimumWidth(400)
+        vbox = QVBoxLayout(self)
+        self.content = QVBoxLayout()
+        self.content.addWidget(QLabel(_('Scanning common paths for existing accounts...')))
+        vbox.addLayout(self.content)
+
+        self.thread = TaskThread(self)
+        self.thread.finished.connect(self.deleteLater) # see #3956
+        network = Network.get_instance()
+        coro = account_discovery(network, self.get_account_xpub)
+        fut = asyncio.run_coroutine_threadsafe(coro, network.asyncio_loop)
+        self.thread.add(
+            fut.result,
+            on_success=self.on_recovery_success,
+            on_error=self.on_recovery_error,
+            cancel=fut.cancel,
+        )
+
+        self.ok_button = OkButton(self)
+        self.ok_button.clicked.connect(self.on_ok_button_click)
+        self.ok_button.setEnabled(False)
+        cancel_button = CancelButton(self)
+        cancel_button.clicked.connect(fut.cancel)
+        vbox.addLayout(Buttons(cancel_button, self.ok_button))
+        self.finished.connect(self.on_finished)
+        self.show()
+
+    def on_finished(self):
+        self.thread.stop()
+
+    def on_ok_button_click(self):
+        item = self.list.currentItem()
+        account = item.data(self.ROLE_ACCOUNT)
+        self.on_account_select(account)
+
+    def on_recovery_success(self, accounts):
+        self.clear_content()
+        if len(accounts) == 0:
+            self.content.addWidget(QLabel(_('No existing accounts found.')))
+            return
+        self.content.addWidget(QLabel(_('Choose an account to restore.')))
+        self.list = QListWidget()
+        for account in accounts:
+            item = QListWidgetItem(account['description'])
+            item.setData(self.ROLE_ACCOUNT, account)
+            self.list.addItem(item)
+        self.list.clicked.connect(lambda: self.ok_button.setEnabled(True))
+        self.content.addWidget(self.list)
+
+    def on_recovery_error(self, exc_info):
+        e = exc_info[1]
+        if isinstance(e, concurrent.futures.CancelledError):
+            return
+        self.clear_content()
+        self.content.addWidget(QLabel(_('Error: Account discovery failed.')))
+        _logger.error(f"recovery error", exc_info=exc_info)
+
+    def clear_content(self):
+        for i in reversed(range(self.content.count())):
+            self.content.itemAt(i).widget().setParent(None)
---
+++ electrum/gui/qt/qrtextedit.py
@@ -0,0 +1,51 @@
+from electrum.i18n import _
+from electrum.plugin import run_hook
+from electrum.simple_config import SimpleConfig
+
+from .util import ButtonsTextEdit, MessageBoxMixin
+
+
+class ShowQRTextEdit(ButtonsTextEdit):
+
+    def __init__(self, text=None, *, config: SimpleConfig):
+        ButtonsTextEdit.__init__(self, text)
+        self.setReadOnly(True)
+        self.add_qr_show_button(config=config)
+        run_hook('show_text_edit', self)
+
+    def contextMenuEvent(self, e):
+        m = self.createStandardContextMenu()
+        m.addAction(_("Show as QR code"), self.on_qr_show_btn)
+        m.exec_(e.globalPos())
+
+
+class ScanQRTextEdit(ButtonsTextEdit, MessageBoxMixin):
+
+    def __init__(self, text="", allow_multi: bool = False, *, config: SimpleConfig):
+        ButtonsTextEdit.__init__(self, text)
+        self.setReadOnly(False)
+        self.add_file_input_button(config=config, show_error=self.show_error)
+        self.add_qr_input_button(config=config, show_error=self.show_error, allow_multi=allow_multi)
+        run_hook('scan_text_edit', self)
+
+    def contextMenuEvent(self, e):
+        m = self.createStandardContextMenu()
+        m.addAction(_("Read QR code"), self.on_qr_input_btn)
+        m.exec_(e.globalPos())
+
+
+class ScanShowQRTextEdit(ButtonsTextEdit, MessageBoxMixin):
+
+    def __init__(self, text="", allow_multi: bool = False, *, config: SimpleConfig):
+        ButtonsTextEdit.__init__(self, text)
+        self.setReadOnly(False)
+        self.add_qr_input_button(config=config, show_error=self.show_error, allow_multi=allow_multi)
+        self.add_qr_show_button(config=config)
+        run_hook('scan_text_edit', self)
+        run_hook('show_text_edit', self)
+
+    def contextMenuEvent(self, e):
+        m = self.createStandardContextMenu()
+        m.addAction(_("Read QR code"), self.on_qr_input_btn)
+        m.addAction(_("Show as QR code"), self.on_qr_show_btn)
+        m.exec_(e.globalPos())
